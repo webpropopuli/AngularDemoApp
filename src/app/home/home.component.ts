2ff9afab60831fa7b748bb6ef320e892576dcb5d
webpropopuli <webpropopuli@gmail.com>
2018-07-25 21:00:34 -0400

    done for today

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,33 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 import { MyDataService } from '../my-data.service';
 
-@Component({
+@Component({ 
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+        ,
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-out', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
-  itemCount: number;
-  addBtnText = 'Add another';
-  goalText = '';
-  goals = []; // todo we're going to load this from store at some point
-
+  itemCount: number = 4;
+  btnText: string = 'Add an Item';
+  goalText: string = 'My first life goal';
+//  goals = [];
+  goals = []; // ['My first life goal', 'I want to climb a mountain', 'Go ice skiing'];
   constructor(private _data: MyDataService) { }
 
   ngOnInit() {
-
     this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
   }
 
   addItem() {
-    if ( this.goalText.length ) {
-      this.goals.push(this.goalText);
-      this.goalText = '';
-      this.itemCount = this.goals.length;
-      }
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
   }
 
